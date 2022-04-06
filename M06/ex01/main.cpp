591384b87a6bac156885634680b3f81f78629363
gilee <gilee@gileeui-MacBookAir.local>
2022-04-06 16:09:40 +0900

    lol

--- M06/ex01/main.cpp
+++ M06/ex01/main.cpp
@@ -2,17 +2,30 @@
 // Created by Giyoung Lee on 3/28/22.
 //
 #include "Caster.hpp"
+typedef struct data {
+    unsigned char x;
+    unsigned int  y;
+    unsigned int  z;
+}Data;
 
-int main(int argc, char** argv){
-	(void)argv;
-	if (argc > 2 || argc == 1) {
-		std::cout << "Error" << std::endl;
-		return 1;
-	}
-	Caster a(argv[1]);
-	a.toChar();
-	a.toInt();
-	a.toFloat();
-	a.toDouble();
+static std::uintptr_t serialize(Data* ptr) {
+  return (*reinterpret_cast<std::uintptr_t*>(ptr));
+};
+
+static Data* desirialize(uintptr_t raw) {
+    std::uintptr_t * tmp = &raw;
+    return (reinterpret_cast<Data*>(tmp));
+};
+
+int main(){
+    Data *k = new Data();
+    k->x = 'k';
+    k->y = 1321313211;
+    k->z = 1232323232;
+    uintptr_t so = serialize(k);
+    std::cout << so << std::endl;
+    std::cout << desirialize(so)->x << std::endl;
+    std::cout << desirialize(so)->y << std::endl;
+    std::cout << desirialize(so)->z << std::endl;
 	return 0;
 }
